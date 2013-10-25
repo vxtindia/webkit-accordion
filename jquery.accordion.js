4c2a372e12f89c538dcf1fb5c4dab5c3fc6f215b
Debjeet Biswas <debjeet@vxtindia.com>
2013-10-25 19:25:15 +0530

    Add setHeight method
    .accordion() now returns a function which can be called anytime to set computed height

--- jquery.accordion.js
+++ jquery.accordion.js
@@ -3,40 +3,46 @@
 		var el = this;
 		var ddHeight;
 		ddHeight = new Array();
-		
+
 		el.addClass('enhance');
-		
-		el.find('dd').each(function(i) {
-			var dd = $(this);
-			ddHeight[i] = dd.height();
-			dd.addClass('closed');
-		});
-		
+		setHeight();
+
+		function setHeight() {
+			el.find('dd').each(function(i) {
+				var dd = $(this);
+
+				dd.css('height', '100%');
+				ddHeight[i] = dd.height();
+				dd.removeAttr('style');
+				dd.addClass('closed');
+			});
+		}
+
 		var hash = location.hash;
 		var hasHash = el.find('dt a[href="'+hash+'"]');
-		
+
 		if (hasHash.length) {
 			var toExpand = hasHash.parent().next('dd');
 			var i = toExpand.index('dd');
-			
+
 			toExpand
 				.attr('id', 'active')
 				.css('height', ddHeight[i]+'px')
 				.removeClass('closed');
 		}
-		
+
 		el.find('dt a').bind('touchstart', function(e) {
 			e.preventDefault();
-			
+
 			var toExpand = $(this).parent().next('dd');
 			var i = toExpand.index('dd');
-			
+
 			if (toExpand.attr('id') == 'active') {
 				toExpand
 					.removeAttr('id')
 					.removeAttr('style')
 					.addClass('closed');
-					
+
 				location.hash = '';
 			} else {
 				var active = toExpand.parent().find('#active');
@@ -52,9 +58,13 @@
 					.attr('id', 'active')
 					.css('height', ddHeight[i]+'px')
 					.removeClass('closed');
-					
+
 				location.hash = $(this).attr('href');
 			}
 		});
+
+		return function() {
+			setHeight();
+		};
 	}
 })(jQuery);
